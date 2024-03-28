57635fb8de623c964ff132c9c2d9f7b4c8ddcd73
sodychoe <sodychoe@icloud.com>
2024-03-28 23:15:21 +0900

    changes

---
+++ pbl8/pbl8/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        MyLinkedList<Integer> integerMyLinkedList = new MyLinkedList<>();
+        MyLinkedList<String> stringMyLinkedList = new MyLinkedList<>();
+        MyLinkedList<Character> characterMyLinkedList = new MyLinkedList<>();
+    }
+}
